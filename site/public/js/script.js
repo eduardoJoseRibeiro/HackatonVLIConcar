15fd7b1b52bd08b9590a30e9a60f8b64a70d14a7
Eduardo Ribeiro <joseribeiroejrs@ufu.br>
2017-11-25 02:11:51 -0200

    Adicionada JS para Equipamentos

--- site/public/js/script.js
+++ site/public/js/script.js
@@ -1,1 +1,7 @@
-console.log("Hello World!");
+function equipamentos(){
+    let equipamento = document.getElementById('equipamento');
+    let index = document.getElementById('index');
+
+    equipamento.style.display = 'block';
+    index.style.display = 'none';
+}
